9a9205979d5f6cdc4e1235b0a3295fd4ca9d42e2
comingaround <131857839+comingaround@users.noreply.github.com>
2024-07-29 16:29:37 +0300

    commit

--- components/Cover/Cover.js
+++ components/Cover/Cover.js
@@ -1,3 +1,16 @@
-export const Cover = (children, background) => {
-    return <div className="">{children}</div>;
-}
+import Image from "next/image";
+
+export const Cover = ({children, background}) => {
+    return (
+        <div className="h-screen text-white bg-slate-800 relative min-h-[400px] flex justify-center items-center">
+            <Image 
+                alt="Cover"
+                src={background}
+                fill
+                className="mix-blend-soft-light object-cover"
+            />
+            <div className="max-w-5xl z-10">{children}</div>
+        </div>
+    );
+};
+
